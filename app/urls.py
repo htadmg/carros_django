5672b207c564774f58e7c526961e0e6fd5df7aca
htadmg <agatadominguestec@hotmail.com>
2024-10-09 08:11:39 -0300

    criando view para adicionar carros, o formulario

--- app/urls.py
+++ app/urls.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import cars_views 
+from cars.views import cars_views, new_car_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', cars_views),
+    path('cars/', cars_views, name='cars_list'),
+    path('new_car/', new_car_views, name='new_car_views'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from cars.models import Car
-
+from cars.forms import CarForm
 
 def cars_views(request):
-    cars = Car.objects.filter(brand=4)
+    cars = Car.objects.all()
+    search = request.GET.get('search')
+    
+    if search:
+        cars = cars = cars.filter(model__contains=search)
 
     return render(
         request,
         'cars.html',
         {'cars' : cars }
     )
+
+
+def new_car_views(request):
+    if request.method == "POST": 
+        pass
+    else:
+        new_car_form = CarForm()
+
+    return render(request, 'new_car.html', {'new_car_form': new_car_form })
