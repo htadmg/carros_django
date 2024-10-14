25711ac8f2fff9668c6a65561a85634a169169cd
htadmg <agatadominguestec@hotmail.com>
2024-10-14 16:40:05 -0300

    criando views para login, logout, registrar usuario

--- app/urls.py
+++ app/urls.py
@@ -4,9 +4,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from cars.views import cars_views, new_car_views
+from accounts.views import register_views, login_views, logout_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', register_views, name='register'),
+    path('login/', login_views, name='login'),
+    path('logout/', logout_views, name='logout'),
     path('cars/', cars_views, name='cars_list'),
     path('new_car/', new_car_views, name='new_car_views'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
