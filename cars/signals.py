bf5bc2acb5cf11096c20eb58a44bd3c9e5ac22b9
htadmg <agatadominguestec@hotmail.com>
2024-10-22 09:23:34 -0300

    criando um pre_save para colocar bio, caso esteja sem.

--- cars/signals.py
+++ cars/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
@@ -14,7 +14,10 @@ def car_inventory_update():
         cars_count=cars_count,
         cars_value=cars_value
     )
-
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        instance.bio = 'Sem detalhes adicionais'
 
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
